279e5f4b195817daf1d3bbe48704faf63b2b7aab
Brian Travis <bztravis88@gmail.com>
2021-08-10 19:49:56 -0400

    ✨ Add constants file content
    CAN IDs, PWM, Sensor DIO, etc.

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -1,10 +1,33 @@
 #pragma once
 
-// drivebase:
-
-static const int
+// drivebase
+const int
     leftLeadDeviceID = 1,
     leftFollowDeviceID = 2,
-    rightLeadDeviceID = 3,
-    rightFollowDeviceID = 4;
+    rightLeadDeviceID = 4,
+    rightFollowDeviceID = 5;
 
+// intake
+const int
+    intakePivotID = 11,
+    intakeWheelsID = 12;
+
+// indexer
+const int
+    indexerBeltsID = 16,
+    indexerFirstSensorDIO = 2,
+    indexerGapSensorDIO = 3;
+
+// shooter
+const int
+    shooterWheelMID = 21,
+    shooterWheelSID = 22,
+    shooterHoodID = 23,
+    shooterKickID = 24,
+    shooterTurretID = 25;
+
+// climb
+const int
+    climbLiftID = 32;
+    // probably not using this but maybe
+    // climbLockPWM = 2;

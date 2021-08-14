c69be5d356ad35f048c0b8e7582aa622da1b6fba
Tananya <tprankprakma@gmail.com>
2021-08-13 20:59:03 -0400

    🚧 Auton skeleton
    Wrote in structure for making autons. Not finished enough to be functional.

--- src/main/cpp/auton/Auton.cpp
+++ src/main/cpp/auton/Auton.cpp
@@ -1,1 +1,136 @@
 #include "auton/Auton.h"
+#include "RobotData.h"
+
+void Auton::RobotInit()
+{
+    //auton selector
+    autonSelector.AddOption("Potato", AutonSelect::autonSelect_potato);
+    autonSelector.SetDefaultOption("Potato", AutonSelect::autonSelect_potato);
+}
+
+void Auton::AutonomousInit(const RobotData &robotData, AutonData &autonData)
+{
+    // making sure the auton sequences start at the right step and setting the auton choice based on dashboard selection
+    autonData.autonStep = 0;
+    autonData.autonSelection = autonSelector.GetSelected();
+    
+}
+
+void Auton::AutonomousPeriodic(const RobotData &robotData, AutonData &autonData, ControllerData &controllerData)
+{
+    // for debugging
+    // frc::SmartDashboard::PutNumber("autonStep", autonData.autonStep);
+    // frc::SmartDashboard::PutNumber("autonSelection", autonData.autonSelection);
+
+    // for autons, the robot MUST be in semi auto mode
+
+    switch(autonData.autonSelection)
+    {
+    case autonSelect_potato:
+        // endAllTasks(robotData, controllerData);
+        break;
+
+    case autonSelect_exitInitLineDriverStation:
+
+        switch (autonData.autonStep)
+        {
+        case 0:
+            // robotData.desiredDBDist = -20;
+            // robotData.driveMode = driveMode_initDriveStraight;
+            break;
+        case 1:
+            // robotData.driveMode = driveMode_driveStraight;
+            break;
+        default:
+            // endAllTasks(robotData);
+            break;
+        }
+        break;
+
+    case autonSelect_exitInitLineRendezvous:
+
+        switch (autonData.autonStep)
+        {
+        case 0:
+            // robotData.desiredDBDist = 20;
+            // robotData.driveMode = driveMode_initDriveStraight;
+            break;
+        case 1:
+            // robotData.driveMode = driveMode_driveStraight;
+            break;
+        default:
+            // endAllTasks(robotData);
+            break;
+        }
+        break;
+
+    case autonSelect_shootAndDriveToDriverStation:
+
+        switch (autonData.autonStep)
+        {
+        case 0:
+            // robotData.sBBtn = true;
+            // startDelay(10, robotData);
+            // robotData.shootingMode = true;
+            // robotData.driveMode = driveMode_potato;
+            // robotData.autonStep++;
+            break;
+        case 1:
+            // checkDelay(robotData);
+            break;
+        case 2:
+            // robotData.shootingMode = false;
+            // robotData.autonStep++;
+            break;
+        case 3:
+            // robotData.desiredDBDist = -20;
+            // robotData.driveMode = driveMode_initDriveStraight;
+            break;
+        case 4:
+            // robotData.driveMode = driveMode_driveStraight;
+            break;
+        default:
+            // endAllTasks(robotData);
+            break;
+        }
+        break;
+
+    case autonSelect_shootAndDriveToRendezvous:
+
+        switch (autonData.autonStep)
+        {
+        case 0:
+            // robotData.sBBtn = true;
+            // startDelay(10, robotData);
+            // robotData.shootingMode = true;
+            // robotData.driveMode = driveMode_potato;
+            // robotData.autonStep++;
+            break;
+        case 1:
+            // checkDelay(robotData);
+            break;
+        case 2:
+            //turn shooter off before driving away
+            // robotData.shootingMode = false;
+            // robotData.autonStep++;
+            break;
+        case 3:
+            // robotData.desiredDBDist = 20;
+            // robotData.driveMode = driveMode_initDriveStraight;
+            break;
+        case 4:
+            // robotData.driveMode = driveMode_driveStraight;
+            break;
+        default:
+            // endAllTasks(robotData);
+            break;
+        }
+        break;
+
+        default:
+            // endAllTasks(robotData);
+            break;
+    }
+
+}
+
--- src/main/include/auton/Auton.h
+++ src/main/include/auton/Auton.h
@@ -1,11 +1,46 @@
 #pragma once
 
+#include <frc/smartdashboard/SendableChooser.h> 
+#include <frc/smartdashboard/SmartDashboard.h>
+
+struct RobotData;
+struct ControllerData;
+
+
 enum AutonSelect
 {
-    autonSelect_potato
+    autonSelect_potato,
+    autonSelect_exitInitLineRendezvous,
+    autonSelect_exitInitLineDriverStation,
+    autonSelect_shootAndDriveToRendezvous,
+    autonSelect_shootAndDriveToDriverStation
+};
+
+enum DriveMode
+{
+    driveMode_teleop,
+    driveMode_potato
 };
 
 struct AutonData
 {
+    int autonStep = 0;
+    AutonSelect autonSelection = autonSelect_potato;
+};
+
+class Auton
+{
+public:
+    void RobotInit();
+    void AutonomousInit(const RobotData &robotData, AutonData &autonData);
+    void AutonomousPeriodic(const RobotData &robotData, AutonData &autonData, ControllerData &controllerData);
+
+private:
+    void startDelay(double duration, const RobotData &robotData);
+    void checkDelay(const RobotData &robotData);
+    void endAllTasks(const RobotData &RobotData, ControllerData &controllerData);
+
+    frc::SendableChooser<AutonSelect> autonSelector;
+
 
 };
